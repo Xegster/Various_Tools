d42afa9593ddd3faafa1a0e5f5ee9dde9dae212b
Rodger S <R.Sherman@gaminglabs.com>
2017-05-23 18:31:37 -0600

    Started the project, then found a better one online already

---
+++ MtG_Tools/MtGTools/MtGTools/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MtGTools
+{
+    class Program
+    {
+        private struct Card
+        {
+            public int Count;
+            public string Name;
+            public Card(int count, string name) { Count = count; Name = name;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Format("Null? - {0}; Type - {1};  Count - {2};", args == null, args == null ? "Null" : args.GetType().ToString(), args == null ? 0 : args.Length));
+            Console.WriteLine(args[0]);
+            var x = Console.ReadLine();
+            var x2 = "";
+
+            var Cards = GetCards(args[0]);
+
+        }
+
+        private static object GetCards(string filePath)
+        {
+            List<Card> Ret = new List<Card>();
+            using (StreamReader sr = new StreamReader(filePath, true))
+            {
+                while (!sr.EndOfStream)
+                    ret.Add(sr.ReadLine());
+            }        
+        }
+    }
+}
